faf48a557e99c1a7ad1a840effc6bf4e4757ee96
Joachim Bargsten <jw@bargsten.org>
2024-02-26 22:50:35 +0100

    Type afterCursor

--- src/query.ts
+++ src/query.ts
@@ -70,7 +70,7 @@ const delay = (ms: number) =>
   });
 
 const hasNextPage = (repos: RepoListPage) => repos?.organization?.repositories?.pageInfo?.hasNextPage ?? true;
-const afterCursor = (repos: any) => repos?.organization?.repositories?.pageInfo?.endCursor ?? null;
+const afterCursor = (repos: RepoListPage) => repos?.organization?.repositories?.pageInfo?.endCursor ?? null;
 
 //  https://docs.github.com/en/graphql/overview/explorer
 export async function* getAllRepos({
